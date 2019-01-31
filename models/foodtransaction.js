bd29524112ac01d04ddf59ade6d1f569d9806487
afitra <loeby45@gmail.com>
2019-01-31 22:08:42 +0700

    cektransaksigagal

--- models/foodtransaction.js
+++ models/foodtransaction.js
@@ -3,10 +3,11 @@ module.exports = (sequelize, DataTypes) => {
   const FoodTransaction = sequelize.define('FoodTransaction', {
     FoodId: DataTypes.INTEGER,
     BuyerId: DataTypes.INTEGER,
-    buyerRating: DataTypes.INTEGER
+    buyerRating: DataTypes.INTEGER,
+    amount: DataTypes.INTEGER
   }, {});
 
-  FoodTransaction.associate = function(models) {
+  FoodTransaction.associate = function (models) {
     FoodTransaction.belongsTo(models.User)
     FoodTransaction.belongsTo(models.Food)
   };
--- routes/buy/buy.js
+++ routes/buy/buy.js
@@ -1,7 +1,38 @@
 const router = require('express').Router();
 const Model = require('../../models');
 
-router.get('/buy', (req,res) => {
+router.get('/', function (req, res) {
+
+    Model.Food.findAll()
+        .then(function (datas) {
+
+            res.render('./buy/buy.ejs', { data: datas })
+            // res.send(datas)
+        })
+        .catch(function (err) {
+            res.send('NOT SOUND')
+        })
+})
+
+
+router.get('/:id', function (req, res) {
+    let obj = {
+        FoodId: req.params.id,
+        BuyerId: 0,
+        buyerRating: 0,
+        amount: 1
+
+    }
+    Model.FoodTransaction.create(obj)
+        .then(function (data) {
+            // res.send(data)
+            res.redirect('/buy')
+
+        })
+        .catch(function (err) {
+            res.send('NOT FOUND')
+        })
+    // res.send(obj)
 })
 
 module.exports = router;
